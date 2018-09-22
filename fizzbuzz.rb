94419973c8730fa08258e77dc67fb9e1c4d4746f
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-22 06:00:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,16 @@
 def fizzbuzz(int)
   if int % 3 == 0   
     fizzbuzz(3) = "Fizz"
-    print fizzbuzz(3)
+    print fizz_3
   end
   
   elsif int % 5 == 0 
     fizzbuzz(5) = "Buzz"
-    print fizzbuzz(5)
+    print fizz_5
+    
+  elsif int % 3 && int % 5 == 0 
+    fizzbuzz(15) = "FizzBuzz"
+    print fizz_15
     
   else 
     ArgumentError
