bcae24ef7595cc5352bbdad13cf70d2383319108
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-22 06:01:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 def fizzbuzz(int)
   if int % 3 == 0   
-    fizzbuzz(3) = "Fizz"
+    fizzbuzz(3) == "Fizz"
     print fizz_3
   end
   
   elsif int % 5 == 0 
-    fizzbuzz(5) = "Buzz"
+    fizzbuzz(5) == "Buzz"
     print fizz_5
     
   elsif int % 3 && int % 5 == 0 
-    fizzbuzz(15) = "FizzBuzz"
+    fizzbuzz(15) == "FizzBuzz"
     print fizz_15
     
   else 
