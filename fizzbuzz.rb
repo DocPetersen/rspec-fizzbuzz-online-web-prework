09b7dd4ddd47b267df87d88f04948893d1550c12
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-22 05:57:13 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,14 @@
-require 'pry'
-
-def fizzbuzz
+def fizzbuzz(int)
   if int % 3 == 0   
-    "Fizz"
-    binding.pry
+    fizzbuzz(3) = "Fizz"
+    print fizzbuzz(3)
+  end
+  
+  elsif int % 5 == 0 
+    fizzbuzz(5) = "Buzz"
+    print fizzbuzz(5)
+    
+  else 
+    ArgumentError
   end
 end
